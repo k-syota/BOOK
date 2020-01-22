032f2bccd3d6461564fa6366bf80211e9965021a
西村翔太 <nishimurasyota@K-Air.local>
2020-01-22 18:18:30 +0900

    add file[Bookers]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,25 @@
 class BooksController < ApplicationController
+  def top  	
+  end
+
   def index
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
 
-  def new
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    render 'show'  
   end
+
+  private
+  def book_params
+  params.require(:book).permit(:title, :body)   
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root "books#top"
   resources :books, only: [:index, :create, :show, :new]
+  resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
